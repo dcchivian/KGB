773295449e89aacad59bc117588411523dff3462
Dylan Chivian <DCChivian@lbl.gov>
2016-09-06 15:03:30 -0700

    debugging

--- KGB.py
+++ KGB.py
@@ -1110,14 +1110,14 @@ def getDomainHits (ContigSet_names, \
                 try:
                     domain_annotation_ref_list = ws.list_objects({'ids':[ws_id],'type':"KBaseGeneFamilies.DomainAnnotation"})
                 except Exception as e:
-                    raise ValueError('Unable to fetch featureSet object from workspace: ' + str(e))
+                    raise ValueError('Unable to list DomainAnnotation objects from workspace: ' + str(e))
                     #to get the full stack trace: traceback.format_exc()
                 for domain_annotation_ref in domain_annotation_ref_list:
                     print ("DomainAnnotation_ref: '"+str(domain_annotation_ref)+"'")  # DEBUG
                     try:
                         domain_data = ws.get_objects([{'ref':str(ws_id)+'/'+str(domain_annotation_ref)}])[0]['data']  
                     except Exception as e:
-                        raise ValueError('Unable to fetch featureSet object from workspace: ' + str(e))
+                        raise ValueError('Unable to fetch DomainAnnotation object from workspace: ' + str(e))
                     #to get the full stack trace: traceback.format_exc()
 
                     # we found the correct DomainAnnotation object
